afde941ea3d9ebf00ee528d57ffdc2bc3c1d07d7
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2022-02-19 15:42:53 -0700

    ...

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
